02826a77e66bd3ca7ab70f212735cd5397e27ad7
huangguizhao <2678383176@qq.com>
2019-06-05 10:12:48 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/springbootgit/controller/OurController.java
@@ -0,0 +1,12 @@
+package com.qianfeng.springbootgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author huangguizhao
+ */
+@Controller
+@RequestMapping("our")
+public class OurController {
+}
